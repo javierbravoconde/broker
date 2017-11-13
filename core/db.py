706a3df4eb7ca28074c542cafe7a2ee7fd3d607e
Javier Bravo Conde <javiebravoconde@gmail.com>
2017-11-12 20:12:04 -0600

    rotate tickers

--- core/db.py
+++ core/db.py
@@ -8,18 +8,59 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
 
+MAX_TICKER_NUM = 15
+
 def saveticker(conn : MongoClient, market, tickerinfo):
+    
+    tickerinfo["Date"] = datetime.datetime.utcnow()
         
     db = conn.markets_db
     markets = db.market_collection
-        
-    tickerinfo["Date"] = datetime.datetime.utcnow()     
     
-    if  markets.find_one( { "Market": market }):
-        markets.update_one({ "Market": market }, {"$push": {"Tickers": tickerinfo}})
+    if  market_exits(conn, market) :
+        number_tickers = get_number_tickers(conn, market)
+        if number_tickers >= MAX_TICKER_NUM : 
+            remove_last_ticker(conn, market)
+            
+        push_ticker_info(conn, market, tickerinfo)
     else:
-        market = { "Market" : market, "Tickers" : [tickerinfo] }
-        markets.insert_one(market).inserted_id
+        create_doc(conn, market, tickerinfo)
+
+def push_ticker_info(conn : MongoClient, market, tickerinfo):
+    db = conn.markets_db
+    markets = db.market_collection
+    markets.update_one({ "Market": market }, {"$push": {"Tickers": tickerinfo}})
+
+
+def create_doc(conn : MongoClient, market, tickerinfo):
+    db = conn.markets_db
+    markets = db.market_collection
+    market_doc = { "Market" : market, "Tickers" : [tickerinfo] }
+    markets.insert_one(market_doc).inserted_id
+
+def market_exits(conn : MongoClient, market):
+    db = conn.markets_db
+    markets = db.market_collection
+    market_docs = list(markets.aggregate( [ { "$match" : { "Market" : market } }, { "$project": { "_id": 0, "Market": 1 } } ] ) )
+    return  len(market_docs) > 0
+
+def get_number_tickers(conn : MongoClient, market):
+    db = conn.markets_db
+    markets = db.market_collection
+    res = markets.aggregate( [ { "$match" : { "Market" : market } }, { "$project": { "_id": 0 , "number_tickers": { "$size": "$Tickers" } } } ] )
+    
+    list_res = list (res)
+    
+    if res:
+        return list_res[0]["number_tickers"]
+    else:
+        return 0
+    
+
+def remove_last_ticker(conn : MongoClient, market):
+    db = conn.markets_db
+    markets = db.market_collection
+    markets.update_one({ "Market": market }, {"$pop": {"Tickers": -1}})
 
 
 def getdocument(conn : MongoClient, market):
--- trader.py
+++ trader.py
@@ -18,6 +18,8 @@ import logging
 from logging import config
 import os
 import json
+from pymongo import MongoClient
+from core import db
 
 def setup_logging(
     default_path='logging.json',
@@ -40,19 +42,24 @@ def setup_logging(
 
 if __name__ == '__main__':
 
-    setup_logging()    
-    logging.getLogger().info("Init app...");
+    #setup_logging()    
+    #logging.getLogger().info("Init app...");
 
 
-    dispatcher = Dispatcher(1)
-    bittrex_feed = BittrexFeed(60, dispatcher)
+    #dispatcher = Dispatcher(1)
+    #bittrex_feed = BittrexFeed(60, dispatcher)
 
-    strategy = DummyStrategy(10, dispatcher)
-    strategy.subscribe_to_ticker(dispatcher, "BTC-LTC")
-    strategy.subscribe_to_ohlcv(dispatcher, "BTC-LTC")
+    #strategy = DummyStrategy(10, dispatcher)
+    #strategy.subscribe_to_ticker(dispatcher, "BTC-LTC")
+    #strategy.subscribe_to_ohlcv(dispatcher, "BTC-LTC")
 
-    testBroker = TestBroker(10, dispatcher)
-    testBroker.subscribe_to_order(dispatcher, "BTC-LTC")
+    
+    #testBroker = TestBroker(10, dispatcher)
+    #testBroker.subscribe_to_order(dispatcher, "BTC-LTC")
+    
+    dbclient = MongoClient()
+    db.saveticker(dbclient, "BTC-BTO" , {'Bid': 0.00774508, 'Ask': 0.00777005, 'Last': 0.00777005})
+    
 
 
     pass
